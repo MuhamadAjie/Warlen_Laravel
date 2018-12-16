4cec002163baf92f44af1fe88d9f4a00316100b6
fakhrimuharram <official77fakhri@gmail.com>
2018-12-16 21:40:38 +0700

    LOGIN BAWAAN + link button

---
+++ resources/views/admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Admin</title>
+</head>
+<body>
+
+	<h2> Selamat datang di halaman Admin :) </h2>
+
+</body>
+</html>
---
+++ resources/views/user.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman User</title>
+</head>
+<body>
+
+	<h2> Ini halaman user </h2>
+
+</body>
+</html>
